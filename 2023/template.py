b231a37c4ec6a382a18b05c346c99ab88aba4d77
Alan Wilson <alan.w.wilson@gmail.com>
2024-12-06 10:45:06 -0800

    Adding code templates

---
+++ 2023/template.py
@@ -0,0 +1,13 @@
+
+
+def part1() -> int:
+    return 0
+
+
+def part2() -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
---
+++ 2023/template_tst.py
@@ -0,0 +1,11 @@
+import pytest
+import day as day
+
+
+@pytest.mark.puzzle
+def test_day_part1():
+    day.part1() == 0
+
+
+def test_day18_part2():
+    day.part2() == 0
