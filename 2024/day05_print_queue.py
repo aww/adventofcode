8d812e7cffae3c721d953f802ff5679b6d3b61b1
Alan Wilson <alan.w.wilson@gmail.com>
2025-02-25 16:26:34 -0800

    Adding day 5, sitting around uncommited since December

---
+++ 2024/day05_print_queue.py
@@ -0,0 +1,108 @@
+from collections import defaultdict
+
+
+def split_order(txt):
+    a, b = txt.strip().split('|')
+    return int(a), int(b)
+
+
+def split_update(txt):
+    seq = txt.strip().split(',')
+    return list([int(x) for x in seq])
+
+
+def read_input(s: str = None) -> list[str]:
+    rows = []
+    if s is None:
+        with open('../private/2024/day05_print_queue_input.txt', 'r') as f:
+            s = f.read()
+    txt_orders, txt_updates = s.strip().split('\n\n')
+    orders = list(map(split_order, txt_orders.splitlines()))
+    updates = list(map(split_update, txt_updates.splitlines()))
+    return orders, updates
+
+
+def middle_number(seq):
+    return seq[len(seq) // 2]
+
+
+
+class OrderRules():
+    def __init__(self, orders):
+        self.after = defaultdict(set)
+        for a,b in orders:
+            self.after[a].add(b)
+
+    def check(self, seq):
+        prev = set()
+        for s in seq:
+            if s in self.after:
+                afters = self.after[s]
+                # print(f"Checking for {s} which must have {afters} only after")
+                if len(afters & prev) > 0:
+                    # print(f"FAILURE: {seq}")
+                    return False
+            prev.add(s)
+        # print(f"Success: {seq}")
+        return True
+
+    def corrected(self, seq):
+        prev = set()  # Tracks what values are previous to the current one
+        is_corrected = False
+        i = 0
+        while(i < len(seq)):
+            s = seq[i]
+            if s in self.after:
+                afters = self.after[s]
+                must_move = afters & prev
+                if len(must_move) > 0:
+                    # There are previous values that must move forward of current value
+                    is_corrected = True
+                    for j in reversed(range(i)):
+                        # Work backward so indexes don't change
+                        # If this one moves pop it, decrement i, and insert it forward
+                        x = seq[j]
+                        if x in must_move:
+                            seq.insert(i, seq.pop(j))
+                            i -= 1
+                            prev.discard(x)  # Update prev, presumes we will move all instances
+            prev.add(s)
+            i += 1
+        if is_corrected:
+            return seq
+        else:
+            return False
+
+
+def sum_middle_correct(orders, updates):
+    rules = OrderRules(orders)
+    sum = 0
+    for u in updates:
+        if rules.check(u):
+            sum += middle_number(u)
+    return sum
+
+
+def sum_middle_corrected(orders, updates):
+    rules = OrderRules(orders)
+    sum = 0
+    for u in updates:
+        corrected = rules.corrected(u)
+        if corrected:
+            sum += middle_number(u)
+    return sum
+
+
+def part1() -> int:
+    orders, updates = read_input()
+    return sum_middle_correct(orders, updates)
+
+
+def part2() -> int:
+    orders, updates = read_input()
+    return sum_middle_corrected(orders, updates)
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
---
+++ 2024/test_day05_print_queue.py
@@ -0,0 +1,68 @@
+import pytest
+import day05_print_queue as day5
+
+
+EXAMPLE_INPUT = """
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+"""
+EXAMPLE_RESULT = 143
+
+
+def test_split_order():
+    assert day5.split_order('47|53') == (47,53)
+    assert day5.split_order('97|13') == (97,13)
+
+
+def test_split_updates():
+    assert day5.split_update('''75,47,61,53,29
+    ''') == [75,47,61,53,29]
+
+
+def test_middle_number():
+    assert day5.middle_number([75,47,61,53,29]) == 61
+    assert day5.middle_number([97,61,53,29,13]) == 53
+    assert day5.middle_number([75,29,13]) == 29
+
+
+def test_order_check():
+    rules = day5.OrderRules([(47,13), (75,47), (97,75)])
+    assert rules.check([75,47,61,53,29])
+    assert rules.check([97,61,53,29,13])
+    assert ~ rules.check([75,97,47,61,53])
+
+
+def test_sum_middle_correct():
+    orders, updates = day5.read_input(EXAMPLE_INPUT)
+    assert day5.sum_middle_correct(orders, updates) == EXAMPLE_RESULT
+
+
+def test_sum_middle_corrected():
+    orders, updates = day5.read_input(EXAMPLE_INPUT)
+    assert day5.sum_middle_corrected(orders, updates) == 123
