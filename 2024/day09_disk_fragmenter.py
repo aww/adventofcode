b587f0eb55d2051aa60baf01367c3577bd4335ae
Alan Wilson <alan.w.wilson@gmail.com>
2025-02-26 12:46:52 -0800

    Day 9 done

--- 2024/day09_disk_fragmenter.py
+++ 2024/day09_disk_fragmenter.py
@@ -1,21 +1,105 @@
-def read_input(s: str | None = None) -> list[str]:
-    rows = []
+import collections
+
+Block = collections.namedtuple("Block", ["pos", "len"])
+Blocks = list[Block]
+
+
+def read_input(s: str | None = None) -> list[int]:
     if s is None:
         with open("../private/2024/day09_disk_fragmenter_input.txt", "r") as f:
             s = f.read()
-    for line in s.strip().splitlines():
-        line = line.strip()
-        if len(line) > 0:
-            rows.append(line)
-    return rows
+    return list(map(int, s.strip()))
+
+
+def expandmap(diskmap: list[int]) -> list[int | None]:
+    expanded: list[int | None] = []
+    for i, n in enumerate(diskmap):
+        if i % 2 == 0:
+            expanded.extend([i // 2] * n)
+        else:
+            expanded.extend([None] * n)
+    return expanded
+
+
+# For Part 2 use a different encoding
+def encodeblocks(diskmap: list[int]) -> tuple[Blocks, Blocks]:
+    files = []
+    empty = []
+    position = 0
+    for i, n in enumerate(diskmap):
+        if i % 2 == 0:
+            files.append(Block(position, n))
+        else:
+            if n != 0:
+                empty.append(Block(position, n))
+        position += n
+    return files, empty
+
+
+def move(expanded: list[int | None]) -> list[int | None]:
+    r: list[int | None] = []
+    j = len(expanded) - 1
+    for i, marker in enumerate(expanded):
+        if j < i:
+            break  # i has moved into the region from which we have moved file blocks
+        if marker is None:
+            while (x := expanded[j]) is None:
+                j -= 1
+            if j <= i:
+                break  # if this happens then at i and beyond it must be all empty
+            r.append(x)
+            j -= 1
+        else:
+            r.append(marker)
+    return r
+
+
+# For Part 2
+def moveblocks(files: Blocks, empties: Blocks) -> tuple[Blocks, Blocks]:
+    files, empties = files.copy(), empties.copy()
+    for i in range(len(files) - 1, -1, -1):
+        for j in range(len(empties)):
+            f, e = files[i], empties[j]
+            if f.pos <= e.pos:
+                break  # no more empty blocks before the file block
+            if f.len <= e.len:
+                files[i] = Block(e.pos, f.len)
+                empties[j] = Block(e.pos + f.len, e.len - f.len)
+                break
+    return files, empties
+
+
+def hash(a: list[int | None]) -> int:
+    h = 0
+    for i, x in enumerate(a):
+        if x is not None:
+            h += i * x
+    return h
+
+
+# For Part 2
+def hashblocks(files: Blocks) -> int:
+    total = 0
+    for i, (position, length) in enumerate(files):
+        # i * (pos + pos + 1 + ... + pos + length-1)
+        total += i * ((length * position) + (length * (length - 1) // 2))
+    return total
 
 
 def part1() -> int:
-    return 0
+    input = read_input()
+    exp = expandmap(input)
+    mv = move(exp)
+    h = hash(mv)
+    return h
 
 
 def part2() -> int:
-    return 0
+    input = read_input()
+    files, empties = encodeblocks(input)
+    files, _ = moveblocks(files, empties)
+    hash = hashblocks(files)
+    return hash
 
 
 if __name__ == "__main__":
--- 2024/test_day09_disk_fragmenter.py
+++ 2024/test_day09_disk_fragmenter.py
@@ -1,11 +1,100 @@
 import pytest
 import day09_disk_fragmenter as day9
 
+B = day9.Block
+
+
+def intnone(x):
+    return None if x == "." else int(x)
+
 
 EXAMPLE_INPUT = """
+2333133121414131402
 """
-EXAMPLE_RESULT = 0
+EXAMPLE_RESULT = 1928
+EXAMPLE_EXPAND_TXT = "00...111...2...333.44.5555.6666.777.888899"
+EXAMPLE_EXPAND = list(map(intnone, EXAMPLE_EXPAND_TXT))
+EXAMPLE_MOVED = list(map(int, "0099811188827773336446555566"))
+
+EXAMPLE_MOVED2 = list(map(intnone, "00992111777.44.333....5555.6666.....8888.."))
+EXAMPLE_RESULT2 = 2858
 
 
-def test_():
-    pass
+def test_expand():
+    diskmap = day9.read_input(EXAMPLE_INPUT)
+    assert day9.expandmap(diskmap) == EXAMPLE_EXPAND
+
+
+def test_move():
+    assert day9.move(EXAMPLE_EXPAND) == EXAMPLE_MOVED
+
+
+def test_hash():
+    assert day9.hash(EXAMPLE_MOVED) == EXAMPLE_RESULT
+    assert day9.hash(EXAMPLE_MOVED2) == EXAMPLE_RESULT2
+
+
+def test_encodeblocks():
+    input = day9.read_input(EXAMPLE_INPUT)
+    print(input)
+    files, empties = day9.encodeblocks(input)
+    assert files == [
+        B(0, 2),
+        B(5, 3),
+        B(11, 1),
+        B(15, 3),
+        B(19, 2),
+        B(22, 4),
+        B(27, 4),
+        B(32, 3),
+        B(36, 4),
+        B(40, 2),
+    ]
+    assert empties == [
+        B(2, 3),
+        B(8, 3),
+        B(12, 3),
+        B(18, 1),
+        B(21, 1),
+        B(26, 1),
+        B(31, 1),
+        B(35, 1),
+    ]
+
+
+def test_moveblocks():
+    input = day9.read_input(EXAMPLE_INPUT)
+    files, empties = day9.encodeblocks(input)
+    files, _ = day9.moveblocks(files, empties)
+    assert files == [
+        B(0, 2),
+        B(5, 3),
+        B(4, 1),
+        B(15, 3),
+        B(12, 2),
+        B(22, 4),
+        B(27, 4),
+        B(8, 3),
+        B(36, 4),
+        B(2, 2),
+    ]
+
+
+def test_hashblocks():
+    assert (
+        day9.hashblocks(
+            [
+                B(0, 2),
+                B(5, 3),
+                B(4, 1),
+                B(15, 3),
+                B(12, 2),
+                B(22, 4),
+                B(27, 4),
+                B(8, 3),
+                B(36, 4),
+                B(2, 2),
+            ]
+        )
+        == EXAMPLE_RESULT2
+    )
